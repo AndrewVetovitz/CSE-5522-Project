0c34c4e349ae42b75e369c214537cc2ee2f64ba5
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-04-12 15:54:59 -0400

    adding pre-train files

--- cifar10/main.py
+++ cifar10/main.py
@@ -1,27 +1,5 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow as tf
-
-import download_cifar10 as download
-import load_cifar10
-
-# path to save model
-path = "image-model.ckpt"
-
 def main():
-    # downloads cifar 10 data
-    download.download()
-
-    # train model
-        # save
-
-    # evaluate
-
-    # print out save path
-    print("Save to", path)
-
+    pass
+    
 if __name__ == '__main__':
     main()
