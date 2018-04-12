89c6ac75d4b7f0bbe80bd12600a3980966c07770
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-04-12 14:07:23 -0400

    readed all download directories for cifar 10 for training, evaluation, data processing

--- cifar10/cifar10_input.py
+++ cifar10/cifar10_input.py
@@ -1,3 +1,20 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Routine for decoding the CIFAR-10 binary file format."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,7 +24,10 @@ import os
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-IMAGE_SIZE = 32
+# Process images of this size. Note that this differs from the original CIFAR
+# image size of 32 x 32. If one alters this number, then the entire model
+# architecture will change and any model would need to be retrained.
+IMAGE_SIZE = 24
 
 # Global constants describing the CIFAR-10 data set.
 NUM_CLASSES = 10
@@ -17,12 +37,15 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
 def read_cifar10(filename_queue):
   """Reads and parses examples from CIFAR10 data files.
+
   Recommendation: if you want N-way read parallelism, call this function
   N times.  This will give you N independent Readers reading different
   files & positions within those files, which will give better mixing of
   examples.
+
   Args:
     filename_queue: A queue of strings with the filenames to read from.
+
   Returns:
     An object representing a single example, with the following fields:
       height: number of rows in the result (32)
@@ -74,9 +97,11 @@ def read_cifar10(filename_queue):
 
   return result
 
+
 def _generate_image_and_label_batch(image, label, min_queue_examples,
                                     batch_size, shuffle):
   """Construct a queued batch of images and labels.
+
   Args:
     image: 3-D Tensor of [height, width, 3] of type.float32.
     label: 1-D Tensor of type.int32
@@ -84,6 +109,7 @@ def _generate_image_and_label_batch(image, label, min_queue_examples,
       in the queue that provides of batches of examples.
     batch_size: Number of images per batch.
     shuffle: boolean indicating whether to use a shuffling queue.
+
   Returns:
     images: Images. 4D tensor of [batch_size, height, width, 3] size.
     labels: Labels. 1D tensor of [batch_size] size.
@@ -113,9 +139,11 @@ def _generate_image_and_label_batch(image, label, min_queue_examples,
 
 def distorted_inputs(data_dir, batch_size):
   """Construct distorted input for CIFAR training using the Reader ops.
+
   Args:
     data_dir: Path to the CIFAR-10 data directory.
     batch_size: Number of images per batch.
+
   Returns:
     images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
     labels: Labels. 1D tensor of [batch_size] size.
@@ -177,10 +205,12 @@ def distorted_inputs(data_dir, batch_size):
 
 def inputs(eval_data, data_dir, batch_size):
   """Construct input for CIFAR evaluation using the Reader ops.
+
   Args:
     eval_data: bool, indicating if one should use the train or eval data set.
     data_dir: Path to the CIFAR-10 data directory.
     batch_size: Number of images per batch.
+
   Returns:
     images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
     labels: Labels. 1D tensor of [batch_size] size.
